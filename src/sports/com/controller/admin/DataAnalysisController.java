12f3f4f10d9a717a88c047333d064fcaeecd864a
jang <wkdchdaud2@naver.com>
2017-06-22 22:08:57 +0900

    R 연동해서 디비에서 값꺼내와서 Sum 때리기 간단한거.

--- src/sports/com/controller/admin/DataAnalysisController.java
+++ src/sports/com/controller/admin/DataAnalysisController.java
@@ -14,20 +14,31 @@ import sports.com.dto.UserDTO;
 import sports.com.service.IAnalysisService;
 import sports.com.service.IUserService;
 import sports.com.util.CmmUtil;
+import sports.com.util.RUtil;
 import sports.com.dto.R_testDTO;
 
+import java.util.List;
 
+@Controller
 public class DataAnalysisController {
 	private Logger log = Logger.getLogger(this.getClass());
 
-	@Resource(name="UserService")
+	@Resource(name="AnalysisService")
 	private IAnalysisService analysisService;
 	
-	@RequestMapping(value="/admin/data_ana",method=RequestMethod.GET)
+	@RequestMapping(value="/admin/Analysis/AnalysisList",method=RequestMethod.GET)
 	public String sales_info(HttpServletRequest request, HttpServletResponse response, ModelMap model) throws Exception
 	{
 		log.info("R PromGramming Start!");
-//		List<R_testDTO> list = analysisService.getAlalysisList();
+		List<R_testDTO> R_list = analysisService.getAnalysisList();
+//		System.out.println("R_list.get(0): "+ R_list.get(0).getPrice());
+		RUtil LJYheartbit = new RUtil();
+		double qwe = LJYheartbit.R_list(R_list);
+		
+		System.out.println("컨트롤러 R 통한 값 ++++ :::: " +qwe); 
+		
+		model.addAttribute("R_test",R_list);
+		
 		
 		return "asd";
 	}
--- src/sports/com/persistance/mapper/AnalysisMapper.java
+++ src/sports/com/persistance/mapper/AnalysisMapper.java
@@ -1,5 +1,13 @@
 package sports.com.persistance.mapper;
 
+import java.util.List;
+
+import config.Mapper;
+import sports.com.dto.R_testDTO;;
+
+@Mapper("AnalysisMapper")
 public interface AnalysisMapper {
 
+	public List<R_testDTO> getAnalysis() throws Exception;
+	
 }
